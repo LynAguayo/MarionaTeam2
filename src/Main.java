2546c5a6f97b086bac374b3340d54596444c7992
marcel <130854048+mfelius@users.noreply.github.com>
2024-10-11 10:19:12 +0200

    tonterias

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        
-    System.out.println("Introdueix un numero: ");
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int num = 0;
+
+        System.out.println("Introdueix un numero: ");
+        num = input.nextInt();
+
+        System.out.println("Has puesto el numero" + num);
 
     // 4t modificación
     }
